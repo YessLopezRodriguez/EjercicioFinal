6f1441bbeaa18d9b54e345cff2915dd0167fe258
YessLopezRodriguez <yes.lopez.ro@gmail.com>
2016-12-07 06:14:22 -0600

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -7,10 +7,12 @@ $(document).ready( function(){
 
 $( ".js-show-make" ).click(function() {
 	$(".page").addClass("make"); //Etapa 8
-
+	$(".js-show-recipe").removeClass("active"); //Etapa 9
+	$(".js-show-make").addClass("active"); //Etapa 9
 });
 
 $( ".js-show-recipe" ).click(function() {
   	$(".page").removeClass("make"); //Etapa 8
-
+  	$(".js-show-recipe").addClass("active"); //Etapa 9
+	$(".js-show-make").removeClass("active"); //Etapa 9
 });
