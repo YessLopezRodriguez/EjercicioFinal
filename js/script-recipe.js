bfd39d15d115f9230d75f734680c25cb9edfee95
YessLopezRodriguez <yes.lopez.ro@gmail.com>
2016-12-07 06:12:42 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,6 +1,16 @@
 $(document).ready( function(){
-	
+
 	console.log("Esta Listo"); //Etapa 7
 	$(".js-menu").hide(); // Etapa 1
+
+});
+
+$( ".js-show-make" ).click(function() {
+	$(".page").addClass("make"); //Etapa 8
+
+});
+
+$( ".js-show-recipe" ).click(function() {
+  	$(".page").removeClass("make"); //Etapa 8
 
 });
